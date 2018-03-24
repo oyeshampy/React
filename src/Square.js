bed9eb5296395438a02b13028b185e6c56ee24ab
Puneet Sharma <puneetsharmakpt@gmail.com>
2018-03-24 11:54:18 +0100

    initial commit

---
+++ src/Square.js
@@ -0,0 +1,17 @@
+import React from 'react';
+class Square extends React.Component {
+    constructor(props){
+      super(props);
+      this.state={
+        value:null,
+      };
+    }
+    render() {
+      return (
+        <button className="square" onClick={()=>this.props.onClick({value:'X'})}>
+      {this.props.value}
+        </button>
+      );
+    }
+  }
+  export default Square;
